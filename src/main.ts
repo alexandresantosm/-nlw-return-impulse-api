aee65e733d7d6f30b76e548f11b2d630620c9db7
Alexandre Menezes <amenezes031@gmail.com>
2022-05-06 12:01:28 -0300

    feat: create feedbacks route

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import express from 'express';
+import { prisma } from './database/prisma';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.send("Hello World");
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
